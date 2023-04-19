8f8b0645367e55c0b33ec8d3db95219357e7f2cc
Kieren Hetherington <98293613+KHetherington89@users.noreply.github.com>
2023-04-19 09:10:14 +0100

    Add basic layout elements and begin setting layout

--- library.js
+++ library.js
@@ -1,1 +1,4 @@
-const temp = 2;
+const footer=document.getElementById('footer');
+
+footer.innerHTML = `<a style="color:black; text-decoration:none" href="https://github.com/KHetherington89/calculator">Copyright © ${new Date().getFullYear()} KHetherington89 
+<i class="fa fa-github" style="font-size:24p; color:black"></i></a>`;
