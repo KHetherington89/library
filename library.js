1d59642e33e1b34c037454f5607dbdb2ef112d5e
Kieren Hetherington <98293613+KHetherington89@users.noreply.github.com>
2023-04-20 11:34:19 +0100

    Add modals to be use for adding books.

--- library.js
+++ library.js
@@ -1,4 +1,9 @@
+const bookCount=document.getElementById('bookCount');
 const footer=document.getElementById('footer');
+
+const numOfBooks = 0;
+
+bookCount.innerHTML = `<p class="bookCountText" id="bookCountText">Books in Library: ${numOfBooks}<p>`
 
 footer.innerHTML = `<a style="color:black; text-decoration:none" href="https://github.com/KHetherington89/calculator">Copyright © ${new Date().getFullYear()} KHetherington89 
 <i class="fa fa-github" style="font-size:24p; color:black"></i></a>`;
